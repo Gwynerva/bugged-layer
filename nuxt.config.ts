ea92ddd9aab72391bab022413431172753de1fb9
Peter Radko <cmtvpetya@gmail.com>
2025-08-16 10:54:37 +0300

    Update to Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
-import { resolve } from 'node:path';
-import { fileURLToPath } from "node:url";
-
 export default defineNuxtConfig({
   $meta: {
     name: 'bugged-layer',
   },
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
-  future: { compatibilityVersion: 4 },
-})
+});
